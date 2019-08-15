a94455d82222b6e20f8c3304dbbf6d37aae3d4f0
Rachell Sincere <rachellsincere@gmail.com>
2019-08-15 19:24:18 -0400

    created middleware using specifications and setup api and server

--- api/server.js
+++ api/server.js
@@ -4,4 +4,75 @@ const server = express();
 
 server.use(express.json());
 
+const userDb = require('./users/userDb.js');
+const postDb = require('./posts/postDb');
+
+//Add Middleware below
+// logger() (Global Middleware)
+function logger(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+    );
+
+    next();
+}
+
+server.use(logger); //invoke logger middleware
+
+//specific to  requests that expect user id param:
+// validateUserId()
+function validateUserId(req, res, next) {
+    const { id } = req.params;
+
+    userDb.getById(id)
+    .then( user => {
+        if (user) {
+            next();
+        } else {
+            res.status(400).json({
+                message: 'invalid user id'
+            })
+        } 
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error processing request'
+        });
+    });
+}
+
+// validateUser()
+function validateUser(req, res, next) {
+    if( req.body && Object.keys(req.body).length > 0) {
+        next();
+    } else if (req.body.name.length === 0 ){
+        res.status(400).json({
+            message: "missing required name field"
+        })
+    } else {
+        res.status(400).json({
+            message: "missing user data" 
+        });
+    }
+}
+
+// validatePost()
+function validatePost(req, res, next) {
+    if( req.body && Object.keys(req.body).length > 0) {
+        next();
+    } else if (req.body.text.length === 0 ){
+        res.status(400).json({
+            message: "missing required text field"
+        })
+    } else {
+        res.status(400).json({
+            message: "missing post data" 
+        });
+    }
+}
+
+//
+
+
 module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const server = require('./api/server.js');
 
+
 server.listen(3500, () => {
     console.log('server is running on port 3500');
 });
